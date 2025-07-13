0b9715a6271e5665db7f1b422b4c17d0e352f13a
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-13 14:48:59 -0300

    feat: consumer delete queue

---
+++ src/main/kotlin/com/dolina_mail_service/consumers/MailConsumer.kt
@@ -0,0 +1,14 @@
+package com.dolina_mail_service.consumers
+
+import com.dolina_mail_service.dtos.DeleteUserMessageDto
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+
+@Component
+class MailConsumer {
+
+    @RabbitListener(queues = ["\${broker.queue.delete.email.name}"])
+    fun listenDeleteQueue(payload: DeleteUserMessageDto) {
+        println(payload)
+    }
+}
