8de810771ca7f8d2d31e210f5124ede1929cdaa8
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-13 14:46:54 -0300

    initial commit

---
+++ src/main/kotlin/com/dolina_mail_service/DolinaMailServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dolina_mail_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DolinaMailServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DolinaMailServiceApplication>(*args)
+}
